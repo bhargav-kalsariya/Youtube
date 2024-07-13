5f6025d395f9227c11f5035146c6cd7f2e2a5f55
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-07-13 11:58:07 +0530

    handled request interceptor

--- client/src/utilities/axiosClient.js
+++ client/src/utilities/axiosClient.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { ACCESS_TOKEN_KEY, getItem } from './localStorage';
 
 export const axiosClient = axios.create({
     baseURL: process.env.REACT_APP_SERVER_BASE_URL,
@@ -7,7 +8,9 @@ export const axiosClient = axios.create({
 
 axiosClient.interceptors.request.use((request) => {
 
-    console.log(request);
+    const accessToken = getItem(ACCESS_TOKEN_KEY);
+    request.headers['Authorization'] = `Bearer ${accessToken}`;
+
     return request;
 
-})
+});
