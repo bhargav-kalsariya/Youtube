527aa9e9e8fcb221308cc169bc784d29ac7f52c1
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-07-16 16:39:27 +0530

    improved header design

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,18 +5,18 @@ import { useSelector } from 'react-redux';
 function Header() {
 
     const myProfile = useSelector(state => state.userReducer?.myProfile);
-
+    const img = 'https://fastly.picsum.photos/id/22/4434/3729.jpg?hmac=fjZdkSMZJNFgsoDh8Qo5zdA_nSGUAWvKLyyqmEt2xs0'
     console.log({ myProfile });
 
     return (
-        <header>
+        <header className='header-wrapper'>
             <div className="logo">MyTube</div>
             <div className="search-bar">
                 <input type="text" placeholder="Search" />
-                <button>Search</button>
+                <button>S</button>
             </div>
             <div className="user-profile">
-                <img src="user-avatar.png" alt="User Avatar" />
+                <img src={img} alt="User Avatar" />
             </div>
         </header>
     );
